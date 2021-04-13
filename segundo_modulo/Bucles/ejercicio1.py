3054c0b5e0678e6d96c707db9c069b761d530ec9
YeiversonYJ <yeiverson.yurgakyj@unac.edu.co>
2021-04-13 18:01:14 -0500

    prueba

--- segundo_modulo/Bucles/ejercicio1.py
+++ segundo_modulo/Bucles/ejercicio1.py
@@ -1,7 +1,7 @@
 """
   Escriba un programa que pida dos números enteros y escriba qué 
     números impares desde el primero hasta el segundo. (+5)
-"""
+
 numero1 = int(input("Digite el primer número entero: "))
 numero2 = int(input("Digite el segundo número entero: "))
 
@@ -9,3 +9,25 @@ for i in range(numero1,numero2+1):
     if i % 2 != 0:
 
         print(i)
+"""
+numero1 = int(input("Digite el primer número entero: "))
+numero2 = int(input("Digite el segundo número entero: "))
+
+for i in range(numero1,numero2+1):
+  if i %  2 == 0:
+    print(i)
+
+def main()
+
+cadena = str(input("Digite una cadena: "))
+conteo = 0
+
+for letra in cadena:
+  if letra in conteo:
+    conteo = conteo + 1
+
+
+   
+
+
+main()
